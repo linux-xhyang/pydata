12863bf29c03c4f76d40c6871e638e5c0de4d12e
yangxiaohe <yangxiaohe@formovie.cn>
2018-05-02 21:09:46 +0800

    update

--- pyfile.py
+++ pyfile.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 import sys, getopt
+import pandas as pd
 
-def file_process(src,dst):
+
+def file_process(src, dst):
     rfile = open(src, "r")
     wfile = open(dst, "w")
     alline = rfile.readlines()
@@ -13,12 +15,13 @@ def file_process(src,dst):
             wfile.write(i)
     wfile.close()
 
+
 def main(argv):
     inputfile = ''
     outputfile = ''
 
     try:
-        opts,args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
         print 'test.py -i <inputfile> -o <outputfile>'
         sys.exit(2)
@@ -32,7 +35,8 @@ def main(argv):
             outputfile = arg
     print 'Input file_process is "', inputfile
     print 'Output file is "', outputfile
-    file_process(inputfile,outputfile)
+    file_process(inputfile, outputfile)
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
