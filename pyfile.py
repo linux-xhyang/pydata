b4694bb572481cfa8b2b6db04719f9caf7f53eb4
yangxiaohe <yangxiaohe@formovie.cn>
2018-05-06 20:22:22 +0800

    update pyfile

--- pyfile.py
+++ pyfile.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
 
-import sys, getopt,re
-import pandas as pd
+import sys, getopt, re
 import json
 import os
 
 rules = {}
+out_dir = ''
+
+
 class rule:
     target = ''
     module = ''
     file = ''
     deps = ''
     command = ''
-    def __init__(self,target,module,file,deps,command):
+
+    def __init__(self, target, module, file, deps, command):
         self.target = target
         self.module = module
         self.file = file
@@ -20,9 +23,18 @@ class rule:
         self.command = command
 
     def tostring(self):
-        return "module :" + self.module + '\n' + "deps :" + self.deps + '\n' + "file :" + self.file + '\n' +"command :" + self.command + '\n'
+        return "module :" + self.module + '\n' + "deps :" + self.deps + '\n' + "file :" + self.file + '\n' + "command :" + self.command + '\n'
+
     def tojson(self):
-        return json.dumps({"directory":os.getenv("PWD"),"command":self.command,"file":self.file},indent=True)
+        global out_dir
+        return json.dumps(
+            {
+                "directory": out_dir,
+                "command": self.command,
+                "file": self.file
+            },
+            indent=True)
+
 
 def file_process(src, dst):
     rfile = open(src, "r")
@@ -35,9 +47,13 @@ def file_process(src, dst):
             wfile.write(i)
     wfile.close()
 
-def rule_parser(src,dst):
-    rfile = open(src, "r")
-    wfile = open(dst, "w")
+
+def rule_parser(dir, file):
+    global out_dir
+    out_dir = dir
+    rfile = open(file, "r")
+    wf = os.path.join(dir, "compile_commands.json")
+    wfile = open(wf, "w")
 
     rule_re = re.compile("rule rule[0-9]+")
 
@@ -50,56 +66,70 @@ def rule_parser(src,dst):
     line = rfile.readline()
     while line:
         if begin == True:
-            if re.match(r" description = ",line):
-                desc = re.split(r"=|:|<= ",line.strip('\n'))
-            elif re.match(r" deps = ",line):
-                deps = re.split(r" = ",line.strip('\n'))
-            elif re.match(r" command = ",line):
-                command = re.split(r" = ",line.strip('\n'))
-                command = re.split("\"\(| \) ",command[1])
+            if re.match(r" description = ", line):
+                desc = re.split(r"=|:|<= ", line.strip('\n'))
+            elif re.match(r" deps = ", line):
+                deps = re.split(r" = ", line.strip('\n'))
+            elif re.match(r" command = ", line):
+                command = re.split(r" = ", line.strip('\n'))
+                command = re.split("\"\(| \) ", command[1])
 
             if desc and deps and command:
                 if deps[1] == "gcc" and len(desc) > 3 and len(command) > 1:
-                    rules[desc[3]] = rule(desc[1],desc[2],desc[3],deps[1],command[1].replace("\$",""))
+                    rules[desc[3]] = rule(desc[1], desc[2], desc[3], deps[1],
+                                          command[1].replace("\$", ""))
                     begin = False
                     desc = None
                     deps = None
                     command = None
 
-        if re.match(rule_re,line):
+        if re.match(rule_re, line):
             desc = None
             deps = None
             command = None
             begin = True
         line = rfile.readline()
     wfile.write("[\n")
-    for key,value in rules.items():
+    for key, value in rules.items():
         wfile.write(value.tojson())
         wfile.write(",\n")
     wfile.write("]")
     wfile.close()
 
+
 def main(argv):
-    inputfile = ''
-    outputfile = ''
+    directory = ''
+    file = ''
 
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+        opts, args = getopt.getopt(argv, "hd:f:", ["dir=", "file="])
     except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputfile>')
+        print('pyfile.py -d <directory> -f <build-file>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile>')
+            print('pyfile.py -d <directory> -f <build-file>')
             sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-    print('Input file_process is "', inputfile)
-    print('Output file is "', outputfile)
-    rule_parser(inputfile, outputfile)
+        elif opt in ("-d", "--dir"):
+            directory = arg
+        elif opt in ("-f", "--file"):
+            file = arg
+
+    if os.path.exists(directory) == True:
+        directory = os.path.abspath(directory)
+        file = os.path.join(directory, file)
+        print('Directory is ', directory)
+        print('File is ', file)
+
+        if os.path.exists(file) == True:
+            rule_parser(directory, file)
+        else:
+            print("compile_commands.json not exists")
+    else:
+        print("directory not exists")
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
+
+#~/src/pydata/pyfile.py -d . -f out/build-anglee.ninja
