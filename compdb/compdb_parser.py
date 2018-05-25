4fecfdd9844892a0e5e34eafa665110b84b98221
yangxiaohe <yangxiaohe@formovie.cn>
2018-05-25 15:35:17 +0800

    fix for kati parse Android.mk file

--- compdb/compdb_parser.py
+++ compdb/compdb_parser.py
@@ -32,7 +32,12 @@ def compdb_parser(dir, file):
                 if r1.search(item['file']):
                     file_count += 1
                     build_cmd = item['command']
-                    build_cmd = re.split(r"PWD=\/proc\/self\/cwd ", build_cmd)
+                    if build_cmd.__contains__('PWD=/proc/self/cwd '):
+                        build_cmd = re.split(r"PWD=\/proc\/self\/cwd ",
+                                             build_cmd)
+                    else:
+                        build_cmd = re.split(r"\/bin\/bash -c \"\(", build_cmd)
+
                     if (len(build_cmd) > 1):
                         build_cmd = build_cmd[1]
                         build_cmd = build_cmd.replace("\t", "")
